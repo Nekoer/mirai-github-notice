79f5c74eeea380777a7a7ad578e8b21c4a112ae5
Nekoer <243462032@qq.com>
2022-04-13 16:05:40 +0800

    fix Image Size

--- src/main/kotlin/utils/CardUtil.kt
+++ src/main/kotlin/utils/CardUtil.kt
@@ -25,17 +25,24 @@ class CardUtil {
         time: String,
         event: Contact
     ): Message {
-//        val contact: String =
-//            "{\"app\":\"com.tencent.structmsg\",\"desc\":\"新闻\",\"view\":\"news\",\"ver\":\"0.0.0.1\",\"prompt\":\"Github更新通知\",\"appID\":\"\",\"sourceName\":\"\",\"actionData\":\"\",\"actionData_A\":\"\",\"sourceUrl\":\"\",\"meta\":{\"news\":{\"app_type\":1,\"appid\":\"100951776\",\"desc\":\"${message}\",\"jumpUrl\":\"${html}\",\"preview\":\"${avatar}\",\"tag\":\"哔哩哔哩\",\"title\":\"${name}推送了\"}},\"config\":{\"autosize\":true,\"ctime\":${Date().time},\"forward\":true,\"type\":\"normal\"},\"text\":\"\",\"extraApps\":[],\"sourceAd\":\"\",\"extra\":\"{\\\"app_type\\\":1,\\\"appid\\\":100951776,\\\"uin\\\":895018766}\"}"
 
         val toExternalResource = ImageUtil.getImage(avatar).toByteArray().toExternalResource()
         val imageId: String = toExternalResource.uploadAsImage(event).imageId
         withContext(Dispatchers.IO) {
             toExternalResource.close()
         }
-//        val message = PlainText("")
-//        return RichMessage.share(url = html, title = name, content = message + "\n${time}", coverUrl = avatar)
-        return Image(imageId).plus(name).plus("\n")
+
+
+        val builder = Image.Builder.newBuilder(imageId)
+        builder.height = 50
+        builder.width = 50
+
+        builder.type = ImageType.PNG
+
+        val image = builder.build();
+
+
+        return image.plus(name).plus("\n")
             .plus("时间：$time").plus("\n")
             .plus("介绍：$message").plus("\n")
             .plus("网址：$html").plus("\n")
